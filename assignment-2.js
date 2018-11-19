9744df1f7c7c3eef59bcd509a575e4863c45c3dc
Anna Hamid <44790380+annahamid@users.noreply.github.com>
2018-11-18 21:42:42 -0500

    Create assignment-2.js

---
+++ assignment-2.js
@@ -0,0 +1,8 @@
+//First, I will declare my variable as an empty string and call it "hash"
+var hash = "";
+//To create this triangle, I will write a loop that makes seven calls to console.log:
+
+ for (i = 0; i <= 7; i++) { //this For loop executes 7 times
+    console.log(hash); // this will produce each new row
+    hash += "#"; //this declares "#" as the constant that gets displayed on each new row, incrementing by one "#" as per the For Loop.
+ }
